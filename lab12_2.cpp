b8e9300ede2212acca86ea3bfad61df9cfc97619
Siiickzer <suphanath_w@cmu.ac.th>
2021-12-21 00:35:32 +0700

    lab12_2 compeleted

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -56,8 +56,7 @@ int main(){
 	int playerScore, yugiScore, playerAction, yugiAction;
 	int playerCards[3] = {drawCard(), drawCard(), 0};
 	int yugiCards[3]= {drawCard(), drawCard(), 0};
-	
-	
+		
 	cout << "---------ORE NO TURN, DRAW!!!------------\n";
 	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
 	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
